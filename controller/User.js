6c923520d2e9391d32bf5d083b504b5d4611ddd5
work-ayank0007 <ayan.khan@adixoo.com>
2025-01-31 12:22:35 +0530

    updating cors

--- controller/User.js
+++ controller/User.js
@@ -51,9 +51,9 @@ const login = async (req, res) => {
 
             return res.status(200).cookie('token', token, {
                 expires: new Date(Date.now()+60*60*1000),
-                // sameSite: 'None',
+                sameSite: 'None',
                 httpOnly: true,
-                // secure:false
+                secure:false
             }
             ).json({
                 status: true,
